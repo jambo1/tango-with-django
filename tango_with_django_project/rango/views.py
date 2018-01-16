64158757de15a816ef6fd0e4fb9c82d1621c0da0
Jamie <j.g1992@live.co.uk>
2018-01-16 22:21:36 +0000

    Fixed views issue

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says hey there partner!")
-    <br/> <br/> <ahref="/rango/about">About</a>
+    <br/> <a href="/rango/about">About</a>
 
 def about(request):
     return HttpResponse("Rango says here is the about page.")
-    <br/> <ahref="/rango/">Index</a>
+    <br/> <a href="/rango/">Index</a>
