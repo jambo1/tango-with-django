712abc857ae25ff5a4ab50533d51179163690d1f
Jamie <j.g1992@live.co.uk>
2018-01-16 22:22:27 +0000

    Fixed views issue

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says hey there partner!")
-    <br/> <a href="/rango/about">About</a>
+    <a href="/rango/about">About</a>
 
 def about(request):
     return HttpResponse("Rango says here is the about page.")
