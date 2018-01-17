f3244db82d9abbaab672408a96a5ead2d8e4c942
Jamie <j.g1992@live.co.uk>
2018-01-17 22:05:37 +0000

    view file fix

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!") <b/>
+    return HttpResponse("Rango says hey there partner!") <br/>
     <a href="/rango/about">About</a>
 
 def about(request):
